57878c30d1e78a0323de59bfcea0ba6fea9e84d2
djawny <djawny@op.pl>
2017-03-04 14:34:20 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/oauth2dropbox/GetDropboxInfoTask.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/oauth2dropbox/GetDropboxInfoTask.java
@@ -4,8 +4,11 @@ import android.os.AsyncTask;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class GetDropboxInfoTask extends AsyncTask<String, Integer, String> {
 
@@ -42,12 +45,19 @@ public class GetDropboxInfoTask extends AsyncTask<String, Integer, String> {
     }
 
     private String sentRequest(String token) throws IOException {
+        MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.dropboxapi.com/2/users/get_current_account";
+        String json = "null";
+
+        RequestBody body = RequestBody.create(jsonMediaType, json);
         Request request = new Request.Builder()
                 .addHeader("Authorization", "Bearer " + token)
                 .addHeader("Content-Type", "application/json")
-                .url("https://api.dropboxapi.com/2/users/get_current_account")
+                .url(url)
+                .post(body)
                 .build();
-        OkHttpClient client = new OkHttpClient();
-        return client.newCall(request).execute().body().string();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
